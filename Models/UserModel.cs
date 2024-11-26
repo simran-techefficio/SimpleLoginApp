cbfe9eea5f202344e74c94367be58a40ade78661
simran-techefficio <91730097+simran-techefficio@users.noreply.github.com>
2024-11-26 17:09:03 +0530

    Add files via upload

---
+++ Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        public string username { get; set; }    
+        public string password { get; set; }   
+    }
+}
